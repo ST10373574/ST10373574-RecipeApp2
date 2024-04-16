8b2747642b209403d25f7548abd68eeef7eff4dd
Darren Dhanasar <ST10373574@vcconnect.edu.za>
2024-04-16 19:04:08 +0200

    Default scaling factor is 1.0 (no scaling)

--- Recipe.cs
+++ Recipe.cs
@@ -5,9 +5,9 @@ class Recipe
 {
     // Properties
     public Ingredient[] Ingredients { get; set; }
-    public Ingredient[] OriginalIngredients { get; set; } // 
+    public Ingredient[] OriginalIngredients { get; set; } //  Store original quantities for resetting
     public Step[] Steps { get; set; }
-    private double ScalingFactor { get; set; } = 1.0; // Default scaling factor is 1.0 (no scaling)
+    private double ScalingFactor { get; set; } = 1.0; // 
 
     // Method to display the recipe
     public void DisplayRecipe()
